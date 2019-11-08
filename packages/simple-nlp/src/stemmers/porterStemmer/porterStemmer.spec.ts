fca4e6d98045ca55eb7aa6847764e3584efa57fc
Viktor <neverbland@Neverblands-MacBook-Pro.local>
2019-11-08 11:03:01 +0000

    Adds more tests

--- packages/simple-nlp/src/stemmers/porterStemmer/porterStemmer.spec.ts
+++ packages/simple-nlp/src/stemmers/porterStemmer/porterStemmer.spec.ts
@@ -7,3 +7,11 @@ test("it should stem run", () => {
 test("it should stem running", () => {
   expect(porterStemFn("running")).toEqual("run");
 });
+
+test("it should stem cut", () => {
+  expect(porterStemFn("cut")).toEqual("cut");
+});
+
+test("it should stem cutting", () => {
+  expect(porterStemFn("cutting")).toEqual("cut");
+});
