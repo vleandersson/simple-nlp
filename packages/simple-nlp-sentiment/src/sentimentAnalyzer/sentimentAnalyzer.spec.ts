5e82aa4971fa94acf8da40d9c8bf77e72580c3ac
Viktor <viktor@neverbland.com>
2020-01-10 07:54:48 +0000

    Adjusts the sentiment analyzer to work correctly

--- packages/simple-nlp-sentiment/src/sentimentAnalyzer/sentimentAnalyzer.spec.ts
+++ packages/simple-nlp-sentiment/src/sentimentAnalyzer/sentimentAnalyzer.spec.ts
@@ -1,4 +1,8 @@
-import { cleanData, aggressiveTokenizerEn } from "simple-nlp/src";
+import {
+  cleanData,
+  aggressiveTokenizerEn,
+  porterStemmer
+} from "simple-nlp/src";
 
 import { SentimentAnalyzer } from "./sentimentAnalyzer";
 import {
@@ -18,6 +22,9 @@ const senticonAnalyzer = new SentimentAnalyzer({
   vocabulary: englishSenticonVoca
 });
 
+senticonAnalyzer.stemVocabulary(porterStemmer);
+patternAnalyzer.stemVocabulary(porterStemmer);
+
 const testConfigurations: {
   analyzer: SentimentAnalyzer;
   language: string;
@@ -30,27 +37,27 @@ const testConfigurations: {
       {
         sentence:
           "So is Gemini a joke, you ask? You're never sure whether Andrew Reed's cinematography is straight borrowing from Michael Mann's cool-blue visuals from Heat or mocking the sort of Angelenos that would watch that heist film and see nothing but a decor schematic for every single interior. Or, for that matter, whether composer Keegan DeWitt's is parodying the sax-heavy scores of Skinemax thrillers past or simply copying one in between icy techno interludes. The fact that the performers, especially the exceptional Kirke, keep their tongues firmly planted in their cheeks helps ground things, as does the fact that the jabs are featherweight enough to pass as shrugs. It may feel insubstantial at times, but somewhere out there, there's a twin of this film that lays on the L.A. Self-Owns Itself mojo in thick clumps. Gemini is the good-sibling version. It's worth a whirl.",
-        score: -0.02696503496503496
+        score: -0.019897186147186146
       },
       {
         sentence:
           "Sadly, stereotypes are this film’s stock in trade. Is Melinda a victim or a warrior or just batshit crazy? The movie can't or won’t decide. Taraji will rise again, she always does. But enduing a full 120 minutes of this shitstorm takes its toll. Bitterness, anger, malice, bad blood – that’s acrimony, baby. And that's what you'll feel if you blow the price of ticket on this hack job.",
-        score: 0.037159420289855076
+        score: 0.0054324324324324345
       },
       {
         sentence:
           "But as soon as Vic decides to hit the road to Knoxville, his birthplace, sentiment infects the film like a virus. Writer-director Adam Rifkin clearly has affection for his star, but he's put him in a leaky vehicle that sinks way before the journey ends. Sam Elliott handled a similar role with more style, emotion and dramatic heft in last year's The Hero. But Reynolds, let's not forget, really is a movie star. And a great one. The pleasure of his company is still an exuberant gift. He deserves more than an opportunity missed.",
-        score: 0.009042553191489356
+        score: -0.0017261904761904719
       },
       {
         sentence:
           "Sadly, stereotypes are this film’s stock in trade. Is Melinda a victim or a warrior or just batshit crazy? The movie can't or won’t decide. Taraji will rise again, she always does. But enduing a full 120 minutes of this shitstorm takes its toll. Bitterness, anger, malice, bad blood – that’s acrimony, baby. And that's what you'll feel if you blow the price of ticket on this hack job.",
-        score: 0.037159420289855076
+        score: 0.0054324324324324345
       },
       {
         sentence:
           'G-Eazy released his expansive third album, The Beautiful & Damned, in December with hit singles "No Limit" and "Him & I." The album featured guest appearances from Cardi B, A$AP Rocky, Charlie Puth and Halsey, and debuted at Number Three on the Billboard 200.',
-        score: 0.04025
+        score: 0.031234848484848487
       }
     ]
   },
@@ -61,22 +68,22 @@ const testConfigurations: {
       {
         sentence:
           "So is Gemini a joke, you ask? You're never sure whether Andrew Reed's cinematography is straight borrowing from Michael Mann's cool-blue visuals from Heat or mocking the sort of Angelenos that would watch that heist film and see nothing but a decor schematic for every single interior. Or, for that matter, whether composer Keegan DeWitt's is parodying the sax-heavy scores of Skinemax thrillers past or simply copying one in between icy techno interludes. The fact that the performers, especially the exceptional Kirke, keep their tongues firmly planted in their cheeks helps ground things, as does the fact that the jabs are featherweight enough to pass as shrugs. It may feel insubstantial at times, but somewhere out there, there's a twin of this film that lays on the L.A. Self-Owns Itself mojo in thick clumps. Gemini is the good-sibling version. It's worth a whirl.",
-        score: -0.007692307692307693
+        score: -0.014935064935064933
       },
       {
         sentence:
           "Sadly, stereotypes are this film’s stock in trade. Is Melinda a victim or a warrior or just batshit crazy? The movie can't or won’t decide. Taraji will rise again, she always does. But enduing a full 120 minutes of this shitstorm takes its toll. Bitterness, anger, malice, bad blood – that’s acrimony, baby. And that's what you'll feel if you blow the price of ticket on this hack job.",
-        score: -0.010144927536231883
+        score: -0.02702702702702703
       },
       {
         sentence:
           "But as soon as Vic decides to hit the road to Knoxville, his birthplace, sentiment infects the film like a virus. Writer-director Adam Rifkin clearly has affection for his star, but he's put him in a leaky vehicle that sinks way before the journey ends. Sam Elliott handled a similar role with more style, emotion and dramatic heft in last year's The Hero. But Reynolds, let's not forget, really is a movie star. And a great one. The pleasure of his company is still an exuberant gift. He deserves more than an opportunity missed.",
-        score: -0.026595744680851064
+        score: -0.021428571428571425
       },
       {
         sentence:
           "Sadly, stereotypes are this film’s stock in trade. Is Melinda a victim or a warrior or just batshit crazy? The movie can't or won’t decide. Taraji will rise again, she always does. But enduing a full 120 minutes of this shitstorm takes its toll. Bitterness, anger, malice, bad blood – that’s acrimony, baby. And that's what you'll feel if you blow the price of ticket on this hack job.",
-        score: -0.010144927536231883
+        score: -0.02702702702702703
       },
       {
         sentence:
@@ -89,16 +96,19 @@ const testConfigurations: {
 
 describe("The sentiment analyzer analyzes the sentiment of sentences in multiple languages using different types of vocabularies", function() {
   testConfigurations.forEach(config => {
-    config.testSentences.forEach(sentencePlusScore => {
+    config.testSentences.forEach(({ sentence, score: _score }) => {
+      // Currently only supports English
+      // TODO: add support for other languages
       const tokens = cleanData({
-        text: sentencePlusScore.sentence,
-        tokenizer: aggressiveTokenizerEn
+        text: sentence,
+        tokenizer: aggressiveTokenizerEn,
+        stemmer: porterStemmer
       });
 
-      const { score, nrHits } = config.analyzer.getSentiment(tokens);
+      const { score } = config.analyzer.getSentiment(tokens);
 
       test(`Should analyze a set of sentences with each configuration (${config.language})`, () => {
-        expect(score).toEqual(sentencePlusScore.score);
+        expect(score).toEqual(_score);
       });
     });
   });
--- packages/simple-nlp-sentiment/src/sentimentAnalyzer/sentimentAnalyzer.ts
+++ packages/simple-nlp-sentiment/src/sentimentAnalyzer/sentimentAnalyzer.ts
@@ -23,6 +23,10 @@ interface VocAfinnOptions extends BaseOptions {
   type: "afinn";
 }
 
+interface GetSentimentOptions {
+  denominator: "tokenLength" | "nrOfHits";
+}
+
 export type SentimentAnalyzerOptions =
   | VocPatternOptions
   | VocSenticonOptions
@@ -30,55 +34,106 @@ export type SentimentAnalyzerOptions =
 
 export class SentimentAnalyzer {
   private negations?: string[];
-  private vocabulary: { [key: string]: number };
+  private vocabulary: Map<string, number>;
+  private __vocabulary: Map<string, number>;
 
   constructor(options: SentimentAnalyzerOptions) {
-    this.vocabulary = {};
+    this.vocabulary = new Map();
     this.negations = options.negations;
 
     switch (options.type) {
       case "senticon":
         Object.keys(options.vocabulary).forEach(word => {
           const polarity = options.vocabulary[word].pol;
-          this.vocabulary[word] = polarity ? parseFloat(polarity) : 0;
+          const parsedPolarity = polarity ? parseFloat(polarity) : 0;
+
+          this.vocabulary.set(word, parsedPolarity);
         });
         break;
 
       case "pattern":
         Object.keys(options.vocabulary).forEach(word => {
           const polarity = options.vocabulary[word].polarity;
+          const parsedPolarity = polarity ? parseFloat(polarity) : 0;
 
-          this.vocabulary[word] = polarity ? parseFloat(polarity) : 0;
+          this.vocabulary.set(word, parsedPolarity);
         });
         break;
 
       case "afinn":
         Object.keys(options.vocabulary).forEach(word => {
-          this.vocabulary[word] = parseFloat(options.vocabulary[word]);
+          const parsedPolarity = parseFloat(options.vocabulary[word]);
+
+          this.vocabulary.set(word, parsedPolarity);
         });
         break;
 
       default:
         throw new Error("Unknown vocabulary type");
     }
+
+    // Store the original version of the vocabulary
+    this.__vocabulary = new Map(this.vocabulary);
   }
 
-  public getSentiment(tokens: string[]) {
+  /**
+   * Stems the stored vocabulary with a given stemmer
+   * This should be done using the same stemmer as used
+   * on the corpus
+   * @param stemmer
+   */
+  public stemVocabulary(stemmer: (text: string[]) => string[]) {
+    const stemmedVocabulary: { [key: string]: number[] } = {};
+    this.vocabulary.clear();
+
+    this.__vocabulary.forEach((score, token) => {
+      const stemmedToken = stemmer([token])[0];
+
+      if (stemmedVocabulary[stemmedToken]) {
+        stemmedVocabulary[stemmedToken].push(score);
+      } else {
+        stemmedVocabulary[stemmedToken] = [score];
+      }
+    });
+
+    // Calculate the mean score for each token
+    Object.keys(stemmedVocabulary).map(key => {
+      const mean =
+        stemmedVocabulary[key].reduce((acc, res) => {
+          return acc + res;
+        }) / stemmedVocabulary[key].length;
+
+      this.vocabulary.set(key, mean);
+    });
+  }
+
+  public getSentiment(tokens: string[], options?: GetSentimentOptions) {
     let score = 0;
     let negator = 1;
     let nrHits = 0;
 
     tokens.forEach(token => {
+      const vocaScore = this.vocabulary.get(token);
+
       if (this.negations && this.negations.indexOf(token) > -1) {
         negator = -1;
         nrHits++;
-      } else if (this.vocabulary[token]) {
-        score += negator * this.vocabulary[token];
+      } else if (vocaScore !== undefined) {
+        score += negator * vocaScore;
         nrHits++;
       }
     });
 
-    score = score / nrHits;
+    switch (options && options.denominator) {
+      case "nrOfHits":
+        score /= nrHits;
+        break;
+
+      case "tokenLength":
+      default:
+        score /= tokens.length;
+        break;
+    }
 
     return { score, nrHits };
   }
