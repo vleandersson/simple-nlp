de024b0100f3b2871134f8f71c320f0aead2691f
Viktor <viktor@neverbland.com>
2020-01-11 15:41:43 +0000

    Updates devtool used for webpack config

--- configs/webpack.config.js
+++ configs/webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 module.exports = {
   entry: { main: "./index.ts" },
-  devtool: "inline-source-map",
+  devtool: "source-map",
   mode: "production",
   module: {
     rules: [
